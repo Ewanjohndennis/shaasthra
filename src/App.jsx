58dfe4717942460dd286fafa2800efc62a7409ff
Ewan John Dennis <ewandennis2006@gmail.com>
2025-06-18 20:14:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
+    setTimeout(() => setLoading(false), 4000);
   }, []);
   const location = window.location;
   if (loading) return <LoadingScreen />;
